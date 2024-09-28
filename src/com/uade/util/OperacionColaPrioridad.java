4206a61de71c3f848c39a865e322a3a715c23b58
Federico Frandsen <fedeandersfrandsen@gmail.com>
2024-09-28 13:25:50 -0300

    hice el segundo metodo, me falta hacer un metodo que copie colas con prioridad a cola para no perder las colas originales cuando hago operaciones

--- src/com/uade/util/OperacionColaPrioridad.java
+++ src/com/uade/util/OperacionColaPrioridad.java
@@ -16,8 +16,8 @@ import org.uade.api.ColaPrioridadTDA;
 
 
 public class OperacionColaPrioridad {
-
-        private ColaPrioridadTDA intercalarColasPrioridad(ColaPrioridadTDA colaPrioridad1, ColaPrioridadTDA colaPrioridad2) {
+        //si quisiera que no se borren las colas originales tendria que hacer unos arreglos agregando colas aux y trabajando sobre ellas.
+        public ColaPrioridadTDA intercalarColasPrioridad(ColaPrioridadTDA colaPrioridad1, ColaPrioridadTDA colaPrioridad2) {
             ColaPrioridadTDA colaPrioridad3 = new ColaPrioridadTDAImpl();
             colaPrioridad3.inicializarCola();
 
@@ -42,5 +42,32 @@ public class OperacionColaPrioridad {
                 }
 
                 return colaPrioridad3;
+        }
+
+        public int largoColaPrioridad(ColaPrioridadTDA colaPrioridad) {
+            int largo = 0;
+            ColaPrioridadTDA colaPrioridadAux = new ColaPrioridadTDAImpl();
+            //colaPrioridadAux.copiarCola(ColaPrioridad);  me falta hacer este metodo
+            while (!colaPrioridadAux.colaVacia()) {
+                colaPrioridadAux.desacolar();
+                largo++;
+            }
+            return largo;
+        }
+       public boolean sonIguales(ColaPrioridadTDA colaPrioridad1, ColaPrioridadTDA colaPrioridad2){
+            int largoCola1 = largoColaPrioridad(colaPrioridad1);
+            int largoCola2 = largoColaPrioridad(colaPrioridad2);
+           //colaPrioridadAux1.copiarCola(ColaPrioridad1)
+           //colaPrioridadAux2.copiarCola(ColaPrioridad2)
+            if (largoCola1 == largoCola2){
+                while (!colaPrioridadAux1.colaVacia()  || !colaPrioridadAAUX2.colaVacia()) {
+                    if (colaPrioridadAux1.primero() == colaPrioridadAux2.primero() || colaPrioridadAux1.prioridad() == colaPrioridadAux2.prioridad()) {
+                        colaPrioridadAux1.desacolar();
+                        colaPrioridadAux2.desacolar();
+                    } else { return false;}
+                }
+
+            }else return false;
+            return true;
         }
 }
