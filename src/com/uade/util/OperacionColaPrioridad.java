099d4fb4a8ef2b18682c42f7dc3548d0d9b42765
Federico Frandsen <fedeandersfrandsen@gmail.com>
2024-09-28 14:34:10 -0300

    hice un metodo para copiar colas con prioridad y asi arreglar el problema de que se me vacien

--- src/com/uade/util/OperacionColaPrioridad.java
+++ src/com/uade/util/OperacionColaPrioridad.java
@@ -47,7 +47,8 @@ public class OperacionColaPrioridad {
         public int largoColaPrioridad(ColaPrioridadTDA colaPrioridad) {
             int largo = 0;
             ColaPrioridadTDA colaPrioridadAux = new ColaPrioridadTDAImpl();
-            //colaPrioridadAux.copiarCola(ColaPrioridad);  me falta hacer este metodo
+            colaPrioridadAux.inicializarCola();
+            copiarCola(colaPrioridad, colaPrioridadAux);
             while (!colaPrioridadAux.colaVacia()) {
                 colaPrioridadAux.desacolar();
                 largo++;
@@ -57,17 +58,43 @@ public class OperacionColaPrioridad {
        public boolean sonIguales(ColaPrioridadTDA colaPrioridad1, ColaPrioridadTDA colaPrioridad2){
             int largoCola1 = largoColaPrioridad(colaPrioridad1);
             int largoCola2 = largoColaPrioridad(colaPrioridad2);
-           //colaPrioridadAux1.copiarCola(ColaPrioridad1)
-           //colaPrioridadAux2.copiarCola(ColaPrioridad2)
-            if (largoCola1 == largoCola2){
-                while (!colaPrioridadAux1.colaVacia()  || !colaPrioridadAAUX2.colaVacia()) {
+
+            ColaPrioridadTDA colaPrioridadAux1 = new ColaPrioridadTDAImpl();
+            ColaPrioridadTDA colaPrioridadAux2 = new ColaPrioridadTDAImpl();
+
+           colaPrioridadAux1.inicializarCola();
+           colaPrioridadAux2.inicializarCola();
+
+           copiarCola(colaPrioridad1,colaPrioridadAux1);
+           copiarCola(colaPrioridad2, colaPrioridadAux2);
+
+           if (largoCola1 == largoCola2){
+                while (!colaPrioridadAux1.colaVacia()  || !colaPrioridadAux2.colaVacia()) {
                     if (colaPrioridadAux1.primero() == colaPrioridadAux2.primero() || colaPrioridadAux1.prioridad() == colaPrioridadAux2.prioridad()) {
                         colaPrioridadAux1.desacolar();
                         colaPrioridadAux2.desacolar();
                     } else { return false;}
                 }
 
-            }else return false;
+            }else {return false;}
             return true;
         }
+
+        public void copiarCola(ColaPrioridadTDA colaOrigen, ColaPrioridadTDA colaDestino ) {
+            ColaPrioridadTDA colaAux = new ColaPrioridadTDAImpl();
+            colaAux.inicializarCola();
+
+            while (!colaOrigen.colaVacia()) {
+                colaAux.acolarPrioridad(colaOrigen.primero(), colaOrigen.primero());
+                colaDestino.acolarPrioridad(colaOrigen.primero(), colaOrigen.primero());
+                colaOrigen.desacolar();
+            }
+            while (!colaAux.colaVacia()) {
+                colaOrigen.acolarPrioridad(colaAux.primero(), colaAux.prioridad());
+                colaAux.desacolar();
+            }
+
+        }
+
+
 }
